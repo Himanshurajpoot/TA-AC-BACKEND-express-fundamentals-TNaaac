4244d7fef46d51562f1500ed4bca6594f6ef2138
Himanshu <harryrajpoot2003@gmail.com>
2024-04-13 21:44:43 +0530

    completed

---
+++ block-BNaabq/sample/server.js
@@ -0,0 +1,39 @@
+let express = require("express");
+let logger = require("morgan")
+let cookie_paesel = require("cookie-parser")
+
+let app = express()
+
+
+
+app.use(logger("dev"))
+
+app.use(cookie_paesel())
+
+
+
+// app.use(express.json())
+
+// app.use(express.urlencoded({extended: false}))
+
+// app.use(express.static())
+
+
+app.use((req,res,next)=>{
+    console.log(req.cookies)
+    next()
+})
+
+app.use("/about",(req,res, next)=>{
+    res.cookie("username", "himanshu")
+    next()
+})
+
+
+app.get("/about",(req,res)=>{
+ res.send("welcome")
+})
+
+app.listen(3000, ()=>{
+    console.log("this server is listen on port 3000")
+})
